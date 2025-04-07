9816fab5d92794fad1264bd51273a09bdb2f7e9b
JanHab <jan.habscheid@rwth-aachen.de>
2025-04-07 11:25:33 +0200

    restructure geometry

--- clean.sh
+++ clean.sh
@@ -10,10 +10,10 @@ cd ..
 
 for angle_vale in 0.091 0.182 0.273 0.364 0.455 0.546
     do 
-        rm -rf sonicFoam/angle_value_$angle_vale
+        rm -rf src/angle_value_$angle_vale
     done
 
 for num_xcells in 10 50 100 200 400 #50 100 200 400
     do 
-        rm -rf sonicFoam/convergence_$num_xcells
+        rm -rf src/convergence_$num_xcells
     done
--- run.sh
+++ run.sh
@@ -9,11 +9,11 @@ foamListTimes -rm
 cd ..
 
 Define grid
-num_xcells=100
-num_ycells=50
+num_xcells=80
+num_ycells=130
 
 # Create all the different angle directories
-for angle_vale in 0.091 0.182 0.273 0.364 0.455 0.546
+for angle_vale in 0.091 0.182 0.273 0.364 0.419 0.455 0.546
     do 
         foamCloneCase sonicFoam angle_value_$angle_vale
         cd angle_value_$angle_vale
@@ -48,39 +48,39 @@ for angle_vale in 0.091 0.182 0.273 0.364 0.455 0.546
         cd ..
     done
 
-# Convergence Analysis for Theta=20
-# Create all the different angle directories
-yangle=0.364
+# # Convergence Analysis for Theta=20
+# # Create all the different angle directories
+# yangle=0.364
 
-for num_xcells in 10 50 100 200 400 #50 100 200 400
-    do 
-        foamCloneCase sonicFoam convergence_$num_xcells
-        cd convergence_$num_xcells
+# for num_xcells in 10 50 100 200 400 #50 100 200 400
+#     do 
+#         foamCloneCase sonicFoam convergence_$num_xcells
+#         cd convergence_$num_xcells
 
-        # Caluclate num_ycells based on num_xcells
-        num_ycells=$(echo "scale=0; $num_xcells * 0.5" | bc)
+#         # Caluclate num_ycells based on num_xcells
+#         num_ycells=$(echo "scale=0; $num_xcells * 0.5" | bc)
 
-        # Create the mesh
-        yangle=$yangle num_xcells=$num_xcells num_ycells=$num_ycells blockMesh > log.blockMesh
+#         # Create the mesh
+#         yangle=$yangle num_xcells=$num_xcells num_ycells=$num_ycells blockMesh > log.blockMesh
 
-        # Check mesh quality
-        checkMesh -allGeometry -allTopology > log.checkMesh
-        echo "✅ Mesh generation completed!"
+#         # Check mesh quality
+#         checkMesh -allGeometry -allTopology > log.checkMesh
+#         echo "✅ Mesh generation completed!"
 
-        # Run the solver
-        echo "Running sonicFoam..."
-        sonicFoam > log.solver
-        # Postprocessing 
-        sonicFoam > log.postProcess
-        if [ $? -ne 0 ]; then
-            echo "❌ sonicFoam failed. Check log.solver for details."
-            exit 1
-        fi
-        echo "✅ Solver completed!"
+#         # Run the solver
+#         echo "Running sonicFoam..."
+#         sonicFoam > log.solver
+#         # Postprocessing 
+#         sonicFoam > log.postProcess
+#         if [ $? -ne 0 ]; then
+#             echo "❌ sonicFoam failed. Check log.solver for details."
+#             exit 1
+#         fi
+#         echo "✅ Solver completed!"
 
-        # Create touch.case for paraview
-        touch case.foam
+#         # Create touch.case for paraview
+#         touch case.foam
 
-        # Move back to the base directory
-        cd ..
-    done
+#         # Move back to the base directory
+#         cd ..
+#     done
