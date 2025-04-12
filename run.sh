6b216be63da55252bfe265f7c389f5f315228cc2
JanHab <jan.habscheid@rwth-aachen.de>
2025-04-12 15:04:55 +0200

    increase domain and update analytical values

--- run.sh
+++ run.sh
@@ -9,59 +9,22 @@ foamListTimes -rm
 cd ..
 
 # Define grid
-# num_xcells=80
-# num_ycells=130
+num_xcells=80
+num_ycells=130
 
-# # Create all the different angle directories
-# for angle_vale in 0.091 0.182 0.273 0.364 0.419 0.455 0.546
-#     do 
-#         foamCloneCase sonicFoam angle_value_$angle_vale
-#         cd angle_value_$angle_vale
-#         # Create the mesh
-#         yangle=$angle_vale num_xcells=$num_xcells num_ycells=$num_ycells blockMesh > log.blockMesh
-
-#         # Check if blockMesh was successful
-#         # if [ $? -ne 0 ]; then
-#         #     echo "❌ blockMesh failed. Check log.blockMesh for details."
-#         #     exit 1
-#         # fi
-
-#         # Check mesh quality
-#         checkMesh -allGeometry -allTopology > log.checkMesh
-#         echo "✅ Mesh generation completed!"
-
-#         # Run the solver
-#         echo "Running sonicFoam..."
-#         sonicFoam > log.solver
-#         # Postprocessing 
-#         sonicFoam > log.postProcess
-#         if [ $? -ne 0 ]; then
-#             echo "❌ sonicFoam failed. Check log.solver for details."
-#             exit 1
-#         fi
-#         echo "✅ Solver completed!"
-
-#         # Create touch.case for paraview
-#         touch case.foam
-
-#         # Move back to the base directory
-#         cd ..
-#     done
-
-# Convergence Analysis for Theta=20
 # Create all the different angle directories
-yangle=0.364
-
-for num_xcells in 50 100 200
+for angle_vale in 0.091 0.182 0.273 0.364 0.419 0.455 0.546
     do 
-        foamCloneCase sonicFoam convergence_$num_xcells
-        cd convergence_$num_xcells
-
-        # Caluclate num_ycells based on num_xcells
-        num_ycells=$(echo "scale=0; $num_xcells * 1.625" | bc)
-
+        foamCloneCase sonicFoam angle_value_$angle_vale
+        cd angle_value_$angle_vale
         # Create the mesh
-        yangle=$yangle num_xcells=$num_xcells num_ycells=$num_ycells blockMesh > log.blockMesh
+        yangle=$angle_vale num_xcells=$num_xcells num_ycells=$num_ycells blockMesh > log.blockMesh
+
+        # Check if blockMesh was successful
+        # if [ $? -ne 0 ]; then
+        #     echo "❌ blockMesh failed. Check log.blockMesh for details."
+        #     exit 1
+        # fi
 
         # Check mesh quality
         checkMesh -allGeometry -allTopology > log.checkMesh
@@ -84,3 +47,40 @@ for num_xcells in 50 100 200
         # Move back to the base directory
         cd ..
     done
+
+# # Convergence Analysis for Theta=20
+# # Create all the different angle directories
+# yangle=0.182 #0.364 # 0.182
+
+# for num_xcells in 50 100 200
+#     do 
+#         foamCloneCase sonicFoam convergence_$num_xcells
+#         cd convergence_$num_xcells
+
+#         # Caluclate num_ycells based on num_xcells
+#         num_ycells=$(echo "scale=0; $num_xcells * 1.625" | bc)
+
+#         # Create the mesh
+#         yangle=$yangle num_xcells=$num_xcells num_ycells=$num_ycells blockMesh > log.blockMesh
+
+#         # Check mesh quality
+#         checkMesh -allGeometry -allTopology > log.checkMesh
+#         echo "✅ Mesh generation completed!"
+
+#         # Run the solver
+#         echo "Running sonicFoam..."
+#         sonicFoam > log.solver
+#         # Postprocessing 
+#         # sonicFoam > log.postProcess
+#         if [ $? -ne 0 ]; then
+#             echo "❌ sonicFoam failed. Check log.solver for details."
+#             exit 1
+#         fi
+#         echo "✅ Solver completed!"
+
+#         # Create touch.case for paraview
+#         touch case.foam
+
+#         # Move back to the base directory
+#         cd ..
+#     done
